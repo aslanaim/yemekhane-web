43efc3c316cf5dd63588b80c67d65a6aa0a984cf
Naim Aslan <naimaslan.ce@gmail.com>
2019-07-17 23:15:03 +0300

    ilk commit

---
+++ src/java/Musteri.java
@@ -0,0 +1,39 @@
+
+public class Musteri {
+    int id;
+    String isim;
+    String soyisim;
+    double bakiye;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getIsim() {
+        return isim;
+    }
+
+    public void setIsim(String isim) {
+        this.isim = isim;
+    }
+
+    public String getSoyisim() {
+        return soyisim;
+    }
+
+    public void setSoyisim(String soyisim) {
+        this.soyisim = soyisim;
+    }
+
+    public double getBakiye() {
+        return bakiye;
+    }
+
+    public void setBakiye(double bakiye) {
+        this.bakiye = bakiye;
+    }
+}
---
+++ src/java/Stok.java
@@ -0,0 +1,32 @@
+
+public class Stok {
+    
+    int id;
+    String urun;
+    int miktar;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUrun() {
+        return urun;
+    }
+
+    public void setUrun(String urun) {
+        this.urun = urun;
+    }
+
+    public int getMiktar() {
+        return miktar;
+    }
+
+    public void setMiktar(int miktar) {
+        this.miktar = miktar;
+    }
+    
+}
